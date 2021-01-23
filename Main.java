19583a489afb127716ace7fdf00a1162917a1673
Angelina Dadiverina <imperfect.sparda@gmail.com>
2021-01-23 21:04:13 +0300

    project added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 125_000;
+        int mile_cost = 20;
+        int accumulated_miles = ticket_price / mile_cost;
+
+        System.out.println(accumulated_miles);
+    }
+}
